d194aad1f9e9bb4f0039dc59d9209a24955f347e
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-11-03 14:42:34 -0300

    Atualização

--- lib/modules/movie_detail/widget/movie_detail_header.dart
+++ lib/modules/movie_detail/widget/movie_detail_header.dart
@@ -1,5 +1,6 @@
 import 'package:appfilmesgetx/models/movie_detail_model.dart';
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MovieDetailHeader extends StatelessWidget {
   final MovieDetailModel? movie;
@@ -19,7 +20,11 @@ class MovieDetailHeader extends StatelessWidget {
             final image = movieData.urlImages[index];
             return Padding(
               padding: const EdgeInsets.all(2),
-              child: Image.network(image),
+              child:  FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                      image: image,
+                      fit: BoxFit.cover,
+              )
             );
           },
         ),
