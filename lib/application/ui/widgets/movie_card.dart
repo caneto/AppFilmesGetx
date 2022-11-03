2af305e3bade077eeaa4be1cbbd61112b0d132df
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-11-03 12:12:19 -0300

    Adicionando melhorias

--- lib/application/ui/widgets/movie_card.dart
+++ lib/application/ui/widgets/movie_card.dart
@@ -4,6 +4,7 @@ import 'package:appfilmesgetx/models/movie_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MovieCard extends StatelessWidget {
   final dataFormat = DateFormat('y');
@@ -33,8 +34,9 @@ class MovieCard extends StatelessWidget {
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(20),
                     clipBehavior: Clip.antiAlias,
-                    child: Image.network(
-                      'https://image.tmdb.org/t/p/w200${movie.posterPath}',
+                    child: FadeInImage.memoryNetwork(
+                      placeholder: kTransparentImage,
+                      image: 'https://image.tmdb.org/t/p/w200${movie.posterPath}',
                       width: 148,
                       height: 184,
                       fit: BoxFit.cover,
